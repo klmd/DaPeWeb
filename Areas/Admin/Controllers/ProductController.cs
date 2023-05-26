e3ae574db348f8919b7e6c1bf2b07e522c87f007
klmd <94202203+klmd@users.noreply.github.com>
2023-05-26 10:17:41 +0200

    v06-uprava tabulek

--- Areas/Admin/Controllers/ProductController.cs
+++ Areas/Admin/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using DaPe.DataAccess.Repository;
 using DaPe.Models;
+using DaPe.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -9,76 +10,137 @@ namespace DaPeWeb.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
-            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties:"Category").ToList();
+            objProductList = _unitOfWork.Product.GetAll(includeProperties: "KindOfProduct").ToList();
             return View(objProductList);
         }
-        public IActionResult Create()
+        public IActionResult Upsert(int? id) //Update + Insert = UpSert
         {
-        IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
-        {
-            Text = u.Name,
-            Value = u.Id.ToString()
-        });
-        IEnumerable<SelectListItem> KindOfProductList = _unitOfWork.TypeOfProduct.GetAll().Select(u => new SelectListItem
-        {
-            Text = u.TypeOfProduct,
-            Value = u.Id.ToString()
-        });
-        ViewBag.CategoryList = CategoryList;
-        ViewBag.KindOfProductList = KindOfProductList;
-        return View();
+            ProductVM productVm = new()
+            {
+                CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                KindOfProductList = _unitOfWork.TypeOfProduct.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.TypeOfProduct,
+                    Value = u.Id.ToString()
+                }),
+                Product = new Product()
+            };
+            if (id == null || id == 0)
+            {
+                //create view
+                return View(productVm);
+            }
+            else
+            {
+                //update view
+                productVm.Product = _unitOfWork.Product.Get(u => u.Id == id);
+                return View(productVm);
+            }
+            
         }
+
         [HttpPost]
-        public IActionResult Create(Product obj)
+        public IActionResult Upsert(ProductVM productVm, IFormFile? file)
         {
-            if (obj.NameOfProduct != null && obj.NameOfProduct.ToLower() == obj.DisplayProductNr.ToString().ToLower())
+            if (productVm.Product.NameOfProduct != null && productVm.Product.NameOfProduct.ToLower() == productVm.Product.DisplayProductNr.ToString().ToLower())
             {
                 ModelState.AddModelError("name", "Číslo productu nemůže být stejné jak jméno produktu");
             }
 
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Add(obj);
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString()+Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
+                    if (!string.IsNullOrEmpty(productVm.Product.ImageUrl))
+                    {
+                        //delete old image
+                        var oldImagePath = Path.Combine(wwwRootPath, productVm.Product.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath,fileName),FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    productVm.Product.ImageUrl = @"\images\product\" + fileName;
+                    if (productVm.Product.Id == null)
+                    {
+                        _unitOfWork.Product.Add(productVm.Product);
+                    }
+                    else
+                    {
+                        _unitOfWork.Product.Update(productVm.Product);
+                    }
+                }
                 _unitOfWork.Save();
                 TempData["success"] = "Produkt byl úspěšně vytvořen";
                 return RedirectToAction("Index");
             }
-            return View();
-        }
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
+            else
             {
-                return NotFound();
+                productVm.CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                });
+                productVm.KindOfProductList = _unitOfWork.TypeOfProduct.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.TypeOfProduct,
+                    Value = u.Id.ToString()
+                });
+                TempData["success"] = "Produkt byl úspěšně aktualizován";
+                return View(productVm);
             }
-            Product productFromDb = _unitOfWork.Product.Get(c => c.Id == id);
-            // níže stejné vyjádření téhož
-            //Category categoryFromDb = _db.Categories.Where(c => c.Id == id).FirstOrDefault();
-            //Category categoryFromDb = _db.Categories.Find(id);
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(productFromDb);
         }
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Update(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Produkt byl úspěšně editován";
-                return RedirectToAction("Index");
-            }
-            return View();
-        }
+        //edit is not needed is part of upsert
+        //public IActionResult Edit(int? id)
+        //{
+        //    if (id == null || id == 0)
+        //    {
+        //        return NotFound();
+        //    }
+        //    Product productFromDb = _unitOfWork.Product.Get(c => c.Id == id);
+        //    // níže stejné vyjádření téhož
+        //    //Category categoryFromDb = _db.Categories.Where(c => c.Id == id).FirstOrDefault();
+        //    //Category categoryFromDb = _db.Categories.Find(id);
+        //    if (productFromDb == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(productFromDb);
+        //}
+        //[HttpPost]
+        //public IActionResult Edit(Product obj)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _unitOfWork.Product.Update(obj);
+        //        _unitOfWork.Save();
+        //        TempData["success"] = "Produkt byl úspěšně editován";
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View();
+        //}
         public IActionResult Delete(int? id)
         {
             if (id == null || id == 0)
@@ -108,5 +170,17 @@ namespace DaPeWeb.Areas.Admin.Controllers
             TempData["success"] = "Produkt byl úspěšně smazán";
             return RedirectToAction("Index");
         }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+            objProductList = _unitOfWork.Product.GetAll(includeProperties: "KindOfProduct").ToList();
+            return Json(new {data = objProductList});
+        }
+
+        #endregion
     }
 }
---
+++ wwwroot/js/product.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url:'/admin/product/getall'},
+        "columns": [
+            { data: 'nameOfProduct', "width": "15%"},
+            { data: 'displayProductNr', "width": "15%" },
+            { data: 'category.name', "width": "10%" },
+            { data: 'kindOfProduct.typeOfProduct', "width": "15%" },
+            { data: 'description', "width": "20%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return <div class="w-75 btn-group" role="group">
+                        <a href="" class="btn btn-primary mx-2">
+                            <i class="bi bi-pen-fill"></i>Edituj
+                        </a>
+                        <a class="btn btn-danger mx-2"><i class="bi bi-trash"> Smaž</i></a>
+                    </div>
+                },
+                "width": "10%"
+            }
+        ]
+    });
+}
