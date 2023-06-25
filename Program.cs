acb0c93737efeb724402e0aad9edfdc9bb7c00f8
dape <danielpetrik@outlook.cz>
2023-06-25 21:54:23 +0200

    V12 - after instaling identity

--- Program.cs
+++ Program.cs
@@ -2,12 +2,15 @@ using DaPe.DataAccess.Data;
 using DaPe.DataAccess.Repository;
 using Microsoft.EntityFrameworkCore;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 /*
 
 mySQL - Doladit
